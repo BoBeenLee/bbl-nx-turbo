d766878e8f619abb485fb98d2f921aef94fde9f0
bobeenlee <globaldev@naver.com>
2023-12-05 07:48:07 +0900

    feat: 설정 수정

--- apps/app/next.config.js
+++ apps/app/next.config.js
@@ -4,8 +4,7 @@ const withPWA = require('next-pwa')({
   dest: 'public'
 })
 
-module.exports = withPWA(
-  withImages({
+module.exports = (withImages({
     reactStrictMode: true,
     swcMinify: true,
     publicRuntimeConfig: {
