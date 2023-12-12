fc0c70b1b7b9b16273acef2838f6a906ab73de83
bobeenlee <globaldev@naver.com>
2023-12-13 07:52:07 +0900

    feat: ignore build error 설정

--- apps/app/next.config.js
+++ apps/app/next.config.js
@@ -6,6 +6,13 @@ const withPWA = require('next-pwa')({
 module.exports = (withImages({
     reactStrictMode: true,
     swcMinify: true,
+    typescript: {
+      // !! WARN !!
+      // Dangerously allow production builds to successfully complete even if
+      // your project has type errors.
+      // !! WARN !!
+      ignoreBuildErrors: true,
+    },
     publicRuntimeConfig: {
       staticFolder: '/public',
     },
